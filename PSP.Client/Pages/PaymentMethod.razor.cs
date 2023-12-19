98386cad335e19f81c1165ed4b92cf0bee54920a
kristinadj <kdjereg@gmail.com>
2023-12-19 12:21:49 +0100

    PSP Client init

---
+++ PSP.Client/Pages/PaymentMethod.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using PSP.Client.DTO.Output;
+using PSP.Client.Services;
+
+namespace PSP.Client.Pages
+{
+    public partial class PaymentMethod
+    {
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        private IApiServices ApiServices { get; set; }
+
+        [Inject]
+        private ISnackbar Snackbar { get; set; }
+
+        [Parameter]
+        public int MerchantId { get; set; }
+
+        private List<PaymentMethodODTO> paymentMethods = new();
+
+        private bool isLoading = false;
+        private bool isRedirectInProgress = false;
+        private int selectedPaymentMethod { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            isLoading = true;
+
+            paymentMethods = await ApiServices.GetPaymentMethodsAsync(MerchantId);
+            if (paymentMethods.Any())
+            {
+                selectedPaymentMethod = paymentMethods.First().PaymentMethodId;
+            }
+
+            isLoading = false;
+        }
+    }
+}
--- PSP.Client/Program.cs
+++ PSP.Client/Program.cs
@@ -1,11 +1,22 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
 using PSP.Client;
+using PSP.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiUrl = builder.Configuration.GetValue<string>("ApiUrl");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl!) });
+
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+});
+
+builder.Services.AddScoped<IApiServices, ApiServices>();
 
 await builder.Build().RunAsync();
