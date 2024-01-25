51f8b73ba072a424bd2d19f34f041b7258163ffa
kristinadj <kdjereg@gmail.com>
2024-01-25 22:33:02 +0100

    Invoices archive for merchant

---
+++ WebShop.Client/Pages/MerchantInvoicesArchive.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using WebShop.Client.Code;
+using WebShop.Client.Services;
+using WebShop.DTO.Output;
+
+namespace WebShop.Client.Pages
+{
+    public partial class MerchantInvoicesArchive
+    {
+        [Inject]
+        private IApiServices ApiServices { get; set; }
+
+        [Inject]
+        protected GlobalUserSettings GlobalSettings { get; set; }
+
+        private bool isLoading = false;
+        private List<InvoiceODTO> invoices = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            isLoading = true;
+            invoices = await ApiServices.GetMerchantInvoicesAsync(GlobalSettings.UserId!);
+            isLoading = false;
+            StateHasChanged();
+        }
+    }
+}
